89b3d5a6d37de80f58615223c71b5c5eab3bedec
daniel1-2 <60857088+daniel1-2@users.noreply.github.com>
2023-03-08 16:36:41 -0600

    Update Flannel Tables.sql

--- Flannel Tables.sql
+++ Flannel Tables.sql
@@ -6,7 +6,9 @@ CREATE TABLE `Users` (
   `UserID` INT NOT NULL,
   `UserName` VARCHAR(100) NOT NULL, 
   `Password` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`UserID`),
+  `FirstName` VARCHAR(100) NOT NULL,
+  `LastName` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`UserID`, `UserName`),
   KEY `UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
