dec666cd28f292ad40f9bee833e5276b751314ed
Gagan Kadadevarmath <gagan2@illinois.edu>
2023-04-25 17:43:21 -0500

    working on visualization

--- frontend/src/pages/Visualize/Visualize.js
+++ frontend/src/pages/Visualize/Visualize.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { VictoryChart, VictoryScatter, VictoryAxis, VictoryTheme, VictoryLabel } from 'victory';
 import axios from 'axios';
 import { CircularProgress } from '@mui/material';
+import * as d3 from "d3";
 
 // const data = [
 // 	{ airport: 'JFK', avgDelay: 10, totalDelays: 50 },
@@ -13,106 +14,134 @@ import { CircularProgress } from '@mui/material';
 // ];
 
 const Visualize = () => {
-	const [curAirportData, setCurAirportData] = useState(null);
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(false)
+  const [curAirportData, setCurAirportData] = useState(null);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-			setLoading(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
 
-				const res = await axios.get(`http://localhost:5001/visualize`);
-				setData(res.data.result);
-				console.log(res.data)
+        const res = await axios.get(`http://localhost:5001/visualize`);
+        setData(res.data.result);
+        console.log(res.data)
 
-				if (res.data.success) {
-					setData(res.data.result);
-				} else {
-					setData([])
-				}
-	
-				setLoading(false);
-			} catch (err) {
-				console.error(err);
-			}
+        if (res.data.success) {
+          setData(res.data.result);
+        } else {
+          setData([])
+        }
 
-		};
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
 
-		fetchData();
-		// setLoading(false);
-	}, []);
-	return (
-		<div>
-			{/* {(!loading && data) ? (<div style={{ width: '80%', height: '80%' }}>
-				<VictoryChart
-					width={900}
-					height={750}
-					domain={{ x: [-60, 60], y: [0, 100] }}
-					theme={VictoryTheme.material}
-				>
-					<VictoryAxis offsetX={-100} style={{ axisLabel: { fontSize: 20, padding: 35 } }} label="Average Delay (min)" />
-					<VictoryAxis style={{ axisLabel: { fontSize: 20, padding: 35 } }} dependentAxis label="Number of Delays" />
-					<VictoryScatter
-						data={data}
-						x="avgDelay"
-						y="numLargeDelays"
-						style={{
-							labels: {
-								fill: "black",
-								fontSize: 12
-							},
-							data: {
-								fill: "#e38e39",
-								stroke: "#e38e39",
-								fillOpacity: 0.7,
-								strokeWidth: 3
-							}
-						}}
-						size={({ airport }) => airport.totalDelays * 0.5}
-						labels={({ airport, idx }) => airport.IATACode}
-						labelComponent={<VictoryLabel dy={4} />}
-						events={[
-							{
-								target: 'labels',
-								eventHandlers: {
-									onMouseOver: (_, { index }) => {
-										setCurAirportData(data[index]);
-										return { active: true };
-									},
-									onMouseLeave: () => {
-										setCurAirportData(null);
-										return { active: false };
-									},
-								},
-							},
-							{
-								target: 'data',
-								eventHandlers: {
-									onMouseOver: (_, { index }) => {
-										setCurAirportData(data[index]);
-										return { active: true };
-									},
-									onMouseLeave: () => {
-										setCurAirportData(null);
-										return { active: false };
-									},
-								},
-							},
-						]}
-					/>
-				</VictoryChart>
-				{curAirportData && (
-					<div>
-						<p>Airport: {curAirportData.IATACode}</p>
-						<p>Name: {curAirportData.AirportName}</p>
-						<p>Avg. Delay: {curAirportData.avgDelay}</p>
-						<p>Total Delays: {curAirportData.totalDelays}</p>
-					</div>
-				)}
-			</div>) : <CircularProgress />} */}
-		</div>
-	);
+    };
+
+    fetchData();
+    console.log(data)
+    // setLoading(false);
+  }, []);
+
+
+  const dataDomain = [0, 5000];
+  const delaysDomain = [0, 10000]
+
+  // Define the range of values you want to map your data to
+  const range = [0, 500];
+
+  // Create a linear scale using d3
+  const linearScale = d3.scaleLinear()
+    .domain(dataDomain)
+    .range(range);
+
+  const delayScale = d3.scaleLinear()
+  .domain(delaysDomain)
+  .range(range);
+
+  return (
+    <div>
+      {(!loading && data.length > 0)
+        ? (<div style={{ width: '100vw', height: '80vh' }}>
+          <VictoryChart
+            width={1500}
+            height={750}
+            domain={{ x: [-20, 100], y: [0, 500] }}
+            theme={VictoryTheme.material}
+            zoom={true}
+          >
+            <VictoryAxis offsetX={-100} style={{ axisLabel: { fontSize: 20, padding: 35 } }} label="Average Delay (min)" />
+            <VictoryAxis style={{ axisLabel: { fontSize: 20, padding: 35 } }} dependentAxis label="Number of Delays" />
+            <VictoryScatter
+              data={data.map(datum => ({
+                ...datum,
+                avgDelay: parseFloat(datum.avgDelay),
+                totalDelays: delayScale(datum.totalDelays),
+                numLargeDelays: linearScale(datum.numLargeDelays)
+              }))}
+              x="avgDelay"
+              y="numLargeDelays"
+              style={{
+                labels: {
+                  fill: "black",
+                  fontSize: 12
+                },
+                data: {
+                  fill: "#e38e39",
+                  stroke: "#e38e39",
+                  fillOpacity: 0.7,
+                  strokeWidth: 3
+                }
+              }}
+              size={({ datum }) => datum.totalDelays * 0.05}
+              minBubbleSize={5}
+              maxBubbleSize={5}
+              labels={({ datum, idx }) => datum.IATACode}
+              labelComponent={<VictoryLabel dy={4} />}
+              events={[
+                {
+                  target: 'labels',
+                  eventHandlers: {
+                    onMouseOver: (_, { index }) => {
+                      setCurAirportData(data[index]);
+                      return { active: true };
+                    },
+                    onMouseLeave: () => {
+                      setCurAirportData(null);
+                      return { active: false };
+                    },
+                  },
+                },
+                {
+                  target: 'data',
+                  eventHandlers: {
+                    onMouseOver: (_, { index }) => {
+                      setCurAirportData(data[index]);
+                      return { active: true };
+                    },
+                    onMouseLeave: () => {
+                      setCurAirportData(null);
+                      return { active: false };
+                    },
+                  },
+                },
+              ]}
+            />
+          </VictoryChart>
+          {curAirportData && (
+            <div>
+              <p>Airport: {curAirportData.IATACode}</p>
+              <p>Name: {curAirportData.AirportName}</p>
+              <p>Avg. Delay: {curAirportData.avgDelay}</p>
+              <p>Total Delays: {curAirportData.totalDelays}</p>
+              <p>Number of Delays Greater than 15 Minutes: {curAirportData.numLargeDelays}</p>
+            </div>
+          )}
+        </div>) : <CircularProgress />}
+    </div>
+  );
 };
 
 export default Visualize;
