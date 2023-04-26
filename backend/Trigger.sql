d689add31a9b1d96fb0621d997ac92ad4467cc42
daniel1-2 <60857088+daniel1-2@users.noreply.github.com>
2023-04-26 00:26:42 -0500

    Update Trigger.sql

--- backend/Trigger.sql
+++ backend/Trigger.sql
@@ -1,27 +1,20 @@
 -- when user with nonexisting username tries to login, an account will be created for them
 -- when user with an existing account tries to login but password is incorrect, an error is generated
 -- when user with existing account logs in with correct password, success message generated
-DELIMITER $$
-CREATE TRIGGER `ExistingUserCheck`
-    BEFORE INSERT ON `Users`
-        FOR EACH ROW
-    BEGIN
-        SET @username = (SELECT UserName FROM Users WHERE UserName = new.UserName)$$
-        -- if the username does not exist in Users
+CREATE DEFINER=`root`@`%` TRIGGER `Users_BEFORE_INSERT` BEFORE INSERT ON `Users` FOR EACH ROW BEGIN
+		SET @username = (SELECT UserName FROM Users WHERE UserName = new.UserName);
         IF @username IS NULL THEN
-            SET @userid = (SELECT count(*) + 1 FROM Users)$$
-            INSERT INTO Users VALUES(@userid, new.UserName, new.Password)$$
-        END IF$$
+            SET @userid = (SELECT count(*) + 1 FROM Users);
+            INSERT INTO Users VALUES(@userid, new.UserName, new.Password, new.FirstName, new.LastName);
+         
         
         ELSE 
-        -- if the username already exists in Users
-            SET @password = (SELECT Password FROM Users WHERE UserName = new.UserName)$$
-            IF @password != new.Password
-                SIGNAL SQLSTATE `45000` SET MESSAGE_TEXT = "Password Is Incorrect. Please Try Again"$$
-            END IF$$
+            SET @password = (SELECT Password FROM Users WHERE UserName = new.UserName);
+            IF @password != new.Password THEN
+                SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Password Is Incorrect. Please Try Again";
+            END IF;
             IF @password = new.Password THEN 
-                SIGNAL SQLSTATE `45000` SET MESSAGE_TEXT = "You Are Successfully Logged In"$$
-            END IF$$
-        END ELSE$$
-    END$$
-DELIMITER ;
+                SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "You Are Successfully Logged In";
+            END IF;
+        END IF;
+END
